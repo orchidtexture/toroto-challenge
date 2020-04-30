81c19e49deaf082ea781b348c406cb774ce5f28d
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-04-30 10:32:23 -0500

    fixes serializers

--- backend/subscriptions/api_v1/serializers.py
+++ backend/subscriptions/api_v1/serializers.py
@@ -2,13 +2,11 @@ from rest_framework import serializers
 
 from subscriptions.models import Subscription
 
-from users.api_v1.serializers import CustomUserSerializer
-
 class SubscriptionSerializer(serializers.ModelSerializer):
     """
     This serializer provides the Subscription fields needed for it's creation
     """
-    user_id = serializers.UUIDField(format='hex')
+    user_id = serializers.UUIDField(format='hex', write_only=True)
 
     class Meta:
         model = Subscription
@@ -19,7 +17,6 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             'co2_tons_per_month',
         )
         extra_kwargs = {
-            'user_id': {'write_only': True},
             'monthly_fee': {'read_only': True},
             'co2_tons_per_month': {'read_only': True}
         }
--- backend/users/api_v1/serializers.py
+++ backend/users/api_v1/serializers.py
@@ -1,11 +1,14 @@
 from rest_framework import serializers
 
+from subscriptions.api_v1.serializers import SubscriptionSerializer
+
 from users.models import CustomUser
 
 class CustomUserSerializer(serializers.ModelSerializer):
     """
     This serializer provides the CustomUser fields needed for it's creation
     """
+    subscription = SubscriptionSerializer(read_only=True)
     class Meta:
         model = CustomUser
         fields = (
@@ -14,7 +17,8 @@ class CustomUserSerializer(serializers.ModelSerializer):
             'email',
             'first_name',
             'last_name',
-            'co2_tons_per_year'
+            'co2_tons_per_year',
+            'subscription'
         )
         extra_kwargs = {
             'username': {'read_only': True},
